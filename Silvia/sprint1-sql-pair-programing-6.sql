d77805f4a548819cbbe16b8d23d154c95290b65c
Silvia-Ag <alfayate.silvia@hotmail.com>
2023-03-02 10:45:27 +0100

    ejercicios de pair con Aranzazu

---
+++ Silvia/sprint1-sql-pair-programing-6.sql
@@ -0,0 +1,66 @@
+ CREATE SCHEMA `tienda_zapatillas`;
+ USE `tienda_zapatillas`;
+ 
+ CREATE TABLE `Zapatillas`(
+ `id_zapatilla` INT NOT NULL AUTO_INCREMENT,
+ `modelo` VARCHAR(45) NOT NULL,
+ `color` VARCHAR(45) NOT NULL,
+ PRIMARY KEY(`id_zapatilla`)
+ );
+    
+CREATE TABLE `Clientes`(
+	`id_cliente` INT NOT NULL AUTO_INCREMENT,
+    `nombre` VARCHAR(45) NOT NULL,
+    `numero_telefono` INT(9) NOT NULL,
+    `email` VARCHAR(45) NOT NULL,
+    `direccion` VARCHAR(45) NOT NULL,
+    `ciudad` VARCHAR(45) NOT NULL,
+    `provincia` VARCHAR(45) NOT NULL,
+    `pais` VARCHAR(45) NOT NULL,
+    `codigo_postal` VARCHAR(45) NOT NULL,    
+    PRIMARY KEY(`id_cliente`)
+);
+    
+CREATE TABLE `Empleados`(
+	`id_empleado` INT NOT NULL AUTO_INCREMENT,
+    `nombre` VARCHAR(45) NOT NULL,
+    `tienda` VARCHAR(45) NOT NULL,
+    `salario` INT NOT NULL,
+    `fecha_incorporacion` DATE NOT NULL,
+    PRIMARY KEY(`id_empleado`)
+);
+
+CREATE TABLE `Facturas`(
+	`id_factura` INT NOT NULL AUTO_INCREMENT,
+    `numero_factura` VARCHAR(45) NOT NULL,
+    `fecha` DATE NOT NULL,
+    `id_zapatilla` INT NOT NULL,
+    `id_empleado` INT NOT NULL,
+    `id_cliente` INT NOT NULL,
+    PRIMARY KEY(`id_factura`),
+    CONSTRAINT `fk_Facturas_Zapatillas`
+		FOREIGN KEY(`id_zapatilla`)
+        REFERENCES `Zapatillas` (`id_zapatilla`) ON DELETE CASCADE ON UPDATE CASCADE,
+	CONSTRAINT `fk_Facturas_Empleados`
+		FOREIGN KEY(`id_empleado`)
+		REFERENCES `Empleados` (`id_empleado`) ON DELETE CASCADE ON UPDATE CASCADE,
+	CONSTRAINT `fk_Facturas_Clientes`
+		FOREIGN KEY(`id_cliente`)
+        REFERENCES `Clientes` (`id_cliente`) ON DELETE CASCADE ON UPDATE CASCADE
+);
+      
+ALTER TABLE `Zapatillas`
+ADD COLUMN `marca` VARCHAR(45),
+ADD COLUMN `talla` INT NOT NULL;
+
+ALTER TABLE `Empleados`
+MODIFY `salario` FLOAT;
+
+ALTER TABLE `Clientes`
+DROP COLUMN `pais`;
+
+ALTER TABLE `Clientes`
+MODIFY `codigo_postal` INT(5) ZEROFILL;
+
+ALTER TABLE `Facturas`
+ADD COLUMN `total` FLOAT;
